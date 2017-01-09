c11f427c06e71555751537d05b26bbee1bb79a02
Erko42 <erik.osterlund@bahnhof.se>
2017-01-09 20:36:54 +0100

    test update

--- app/src/main/java/com/example/erik/quest_for_glory/MainActivity.java
+++ app/src/main/java/com/example/erik/quest_for_glory/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     public void test()
     {
         boolean isTrue = true;
-        boolean isFalse = false;
+        boolean isFaalse = false;
     }
 }
